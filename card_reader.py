19aad18f4b1203c5ebc7bfc331992f435391191b
utsushiiro <ozeki.haru@gmail.com>
2018-04-07 08:41:46 +0900

    読み取ったデータをpostする機能を実装

--- card_reader.py
+++ card_reader.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+import os
+import sys
 import binascii
 import nfc
+import json
+import requests
 
 class CardReader:
+	def __init__(self):
+		post_url = os.environ.get('POST_URL') 
+		print(post_url)
+		if post_url is None:
+			print('POST_URL(environment variable) is undefined', file=sys.stderr)
+			sys.exit()
+		self.post_url = post_url
+
 	def on_connect(self, tag):
-		print "   touched"
+		print('   touched')
 		self.idm = binascii.hexlify(tag.idm)
+		self.post_data()
 		return True
+	
+	def post_data(self):
+		data = {
+			'idm':  self.idm
+		}
+		response = requests.post(self.post_url, json=data)
+		print('response status code: ' + str(response.status_code))
 
 	def read_id(self):
 		clf = nfc.ContactlessFrontend('usb')
@@ -16,10 +37,11 @@ class CardReader:
 		finally:
 			clf.close()
 
+
 if __name__ == '__main__':
 	reader = CardReader()
 	while True:
-		print "** waiting for a tag **"
+		print('** waiting for a tag **')
 		reader.read_id()
-		print "   idm: " + reader.idm
-		print "   released"
+		print('   idm: ' + reader.idm)
+		print('   released')
